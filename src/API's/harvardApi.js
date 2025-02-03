44a221590f689dcae253443797baf2ff33a5c44a
LivEva <guests@hotmail.co.uk>
2025-02-03 12:19:21 +0000

    Adding successful axios connection with first api

--- src/API's/harvardApi.js
+++ src/API's/harvardApi.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
-const api = axios.create({baseURL: `https://api.harvardartmuseums.org/RESOURCE_TYPE?apikey=d6e343a5-b715-415e-a623-b2e7dbced8c2`});
+const api = axios.create({
+    baseURL: "https://api.harvardartmuseums.org/",
+params: {
+    apikey: `5057e844-28d1-4bc1-acfe-1b9f6d27807e`
+}});
 
 
 const collectionList = () => {
-    return api.get(`/api`).then((response) => {
+    return api.get(`/object`).then((response) => {
 
         console.log(response.data)
 
         return response.data;
         
+    }).catch((error) => {
+        console.log(error)
+        return error;
     })
 }
 
--- src/components/pages/ExhibitionCollection.jsx
+++ src/components/pages/ExhibitionCollection.jsx
@@ -1,7 +1,22 @@
 import collectionList from "../../API's/harvardApi";
+import { useEffect, useState } from "react";
 
 const ExhibitionCollection = () => {
-	return;
+	const [collections, setCollections] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		collectionList().then((response) => {
+			console.log(response.records);
+			setIsLoading(true);
+			setCollections(response.records);
+			setIsLoading(false);
+		});
+	}, []);
+
+	if (isLoading) {
+		return <h2>Loading Collections...</h2>;
+	}
 };
 
 export default ExhibitionCollection;
