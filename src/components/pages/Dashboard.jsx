7f84120986a4828c51e3133e54868f3559e2f9cf
Olivia Guest <guests@hotmail.co.uk>
2025-05-19 17:25:06 +0100

    Adding new home page layout and additional color schemes

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,15 +1,23 @@
 import SearchArtworks from "../main/SearchArtworks";
 import '../styling/dashboard.css'
+import '../styling/header.css'
 import flowers from '../../images/flowers.jpeg'
 import lady from '../../images/lady.jpeg'
 import japaneseArt from '../../images/japaneseArt.jpeg'
+import logo from '../../images/logo.png'
 
 const Dashboard = () => {
 
     return (
         <div className="dashboard-container">
 
+          <div className="header-section">
+
 <h1>Discover Art & Antiquities Like Never Before...</h1>
+
+<img  className="logo" src={logo} alt="" />
+
+</div>
 
 <div className="search-bar">
 
@@ -22,18 +30,21 @@ const Dashboard = () => {
 <div className="lower-content">
 
 <div className="feature1">
+
+<p>Designed for curious minds—students, researchers, and enthusiasts alike—our platform offers powerful search tools and interactive displays to explore the past in new ways.</p>
   
     <img src={flowers} alt="A glass vase, containing a reflection of the window and light entering from the left, sitting on the edge of a marble topped table and containing a bouquet of various flowers and a stalk of wheat with several insects among the blooms" />
 
-    <p>Step into immersive virtual exhibitions combining fine art and ancient artifacts.</p>
+
 
     </div>
 
   <div className="feature2">
 
     <img src={lady} alt="A woman sitting for a painting, posing among the trees with a book on her lap and a flower in her hand. " />
-    
-     <p>Designed for curious minds—students, researchers, and enthusiasts alike—our platform offers powerful search tools and interactive displays to explore the past in new ways.</p>
+
+    <p>Step into immersive virtual exhibitions combining fine art and ancient artifacts.</p>
+
 
   </div>
 
